acdef8580f2d5601c0e9d13d4a8beb8daa58ddf0
Marlon Rondinelli <mdrondinelli@gmail.com>
2019-05-07 07:19:13 -0700

    Add erosion

---
+++ src/main/java/me/marlon/game/ErosionParticle.java
@@ -0,0 +1,79 @@
+package me.marlon.game;
+
+import me.marlon.gfx.Terrain;
+import org.joml.Vector2f;
+
+public class ErosionParticle {
+    public static final float INERTIA = 0.1f;
+    public static final float CAPACITY = 1.0f;
+    public static final float DEPOSITION = 0.01f;
+    public static final float EROSION = 0.8f;
+    public static final float EVAPORATION = 0.05f;
+    public static final float RADIUS = 4.0f;
+    public static final float MIN_SLOPE = 0.01f;
+    public static final float GRAVITY = -4.0f;
+
+    private Vector2f position;
+    private Vector2f direction;
+    private float speed;
+    private float water;
+    private float sediment;
+
+    public ErosionParticle(Vector2f position, float water) {
+        this.position = position;
+        this.direction = new Vector2f();
+        this.speed = 0.0f;
+        this.water = water;
+        this.sediment = 0.0f;
+    }
+
+    public void update(Terrain terrain) {
+        if (water <= 0.0f)
+            return;
+
+        Vector2f oldPosition = new Vector2f(position);
+
+        float hOld = terrain.sample(position.x, position.y);
+
+        Vector2f gradient = terrain.gradient(position.x, position.y);
+        direction.mul(INERTIA).sub(gradient.mul(1.0f - INERTIA));
+        if (direction.x == 0.0f && direction.y == 0.0f) {
+            float angle = (float) (2.0f * Math.PI * Math.random());
+            direction.x = (float) Math.cos(angle);
+            direction.y = (float) Math.sin(angle);
+        }
+
+        direction.normalize();
+        position.add(direction);
+
+        float hNew = terrain.sample(position.x, position.y);
+        float hDif = hNew - hOld;
+
+        if (hDif > 0.0f) {
+            if (sediment >= hDif) {
+                terrain.deposit(oldPosition.x, oldPosition.y, hDif * 0.25f);
+                sediment -= hDif;
+            } else {
+                terrain.deposit(oldPosition.x, oldPosition.y, sediment * 0.25f);
+                sediment = 0.0f;
+            }
+        } else if (hDif < 0.0f) {
+            float c = Math.max(-hDif, MIN_SLOPE) * speed * water * CAPACITY;
+
+            if (sediment > c) {
+                float amt = (sediment - c) * DEPOSITION;
+                sediment -= amt;
+
+                terrain.deposit(oldPosition.x, oldPosition.y, amt);
+            } else if (sediment < c) {
+                float amt = Math.min((c - sediment) * EROSION, -hDif);
+                sediment += amt;
+
+                terrain.erode(oldPosition.x, oldPosition.y, RADIUS, amt);
+            }
+        }
+
+        speed = (float) Math.sqrt(Math.max(speed * speed + hDif * GRAVITY, 0.0f));
+        water *= 1.0f - EVAPORATION;
+    }
+}
--- src/main/java/me/marlon/gfx/Terrain.java
+++ src/main/java/me/marlon/gfx/Terrain.java
@@ -3,6 +3,7 @@ package me.marlon.gfx;
 import static org.lwjgl.opengl.GL45.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
+import me.marlon.game.ErosionParticle;
 import me.marlon.util.OpenSimplexOctaves;
 import org.joml.Vector2f;
 import org.joml.Vector3f;
@@ -22,7 +23,7 @@ public class Terrain implements AutoCloseable {
         this.size = size;
         heightmap = new float[size][size];
 
-        OpenSimplexOctaves noise = new OpenSimplexOctaves(42.0f, 0.5f, 1.9f,  6);
+        OpenSimplexOctaves noise = new OpenSimplexOctaves(48.0f, 0.5f, 2.0f,  16);
 
         float minHeight = Float.MAX_VALUE;
         float maxHeight = Float.MIN_VALUE;
@@ -34,30 +35,49 @@ public class Terrain implements AutoCloseable {
             }
         }
 
-        float[][] blurred = new float[size][size];
         for (int i = 0; i < size; ++i) {
             for (int j = 0; j < size; ++j) {
                 heightmap[i][j] = (heightmap[i][j] - minHeight) / (maxHeight - minHeight);
-                heightmap[i][j] = (float) Math.pow(heightmap[i][j], 2.3f);
+                float distance = Vector2f.distance(size * 0.5f, size * 0.5f, i, j);
+                float mask = 1.0f - (float) Math.pow(Math.min(1.5f * distance / (size * 0.5f), 1.0f), 2.0f);
+                heightmap[i][j] *= mask;
+                // heightmap[i][j] *= 48.0f;
+//                heightmap[i][j] = (float) Math.pow(heightmap[i][j], 2.3f);
             }
         }
 
-        for (int i = 0; i < size; ++i)
-            for (int j = 0; j < size; ++j)
-                blurred[i][j] = blurSample(i, j, (1.0f - heightmap[i][j]) * 0.5f);
+        for (int i = 0; i < size * size * 8; ++i) {
+            ErosionParticle erosionParticle = new ErosionParticle(new Vector2f((float) Math.random() * size, (float) Math.random() * size), 1.0f);
+            for (int j = 0; j < 30; ++j) {
+                erosionParticle.update(this);
+            }
+        }
 
         for (int i = 0; i < size; ++i) {
             for (int j = 0; j < size; ++j) {
-                heightmap[i][j] = blurred[i][j];
-                float distance = Vector2f.distance(size * 0.5f, size * 0.5f, i, j);
-                float mask = 1.0f - (float) Math.pow(Math.min(1.5f * distance / (size * 0.5f), 1.0f), 3.0f);
-                heightmap[i][j] *= mask;
-                heightmap[i][j] *= 40.0f;
-                heightmap[i][j] = Math.max(heightmap[i][j], 3.0f);
+                 heightmap[i][j] *= 48.0f;
+                 heightmap[i][j] = Math.max(heightmap[i][j], 4.0f);
+//                heightmap[i][j] = (float) Math.pow(heightmap[i][j], 2.3f);
             }
         }
 
-        OpenSimplexOctaves layerNoise = new OpenSimplexOctaves(8.0f, 0.5f, 2.0f, 4);
+//        float[][] blurred = new float[size][size];
+//        for (int i = 0; i < size; ++i)
+//            for (int j = 0; j < size; ++j)
+//                blurred[i][j] = blurSample(i, j, (1.0f - heightmap[i][j]) * 0.5f);
+
+//        for (int i = 0; i < size; ++i) {
+//            for (int j = 0; j < size; ++j) {
+//                heightmap[i][j] = blurred[i][j];
+//                float distance = Vector2f.distance(size * 0.5f, size * 0.5f, i, j);
+//                float mask = 1.0f - (float) Math.pow(Math.min(1.5f * distance / (size * 0.5f), 1.0f), 3.0f);
+//                heightmap[i][j] *= mask;
+//                heightmap[i][j] *= 40.0f;
+//                heightmap[i][j] = Math.max(heightmap[i][j], 3.0f);
+//            }
+//        }
+
+        OpenSimplexOctaves layerNoise = new OpenSimplexOctaves(24.0f, 0.5f, 2.0f, 4);
         ArrayList<Vector3f> vertices = new ArrayList<>();
 
         for (int i = 0; i < size - 1; ++i) {
@@ -71,14 +91,14 @@ public class Terrain implements AutoCloseable {
                 Vector3f n1 = p2.sub(p0, new Vector3f()).cross(p3.sub(p0, new Vector3f())).normalize();
 
                 float y = (p0.y + p1.y + p2.y + p3.y) / 4.0f;
-                if (y > 3.0f)
-                    y += layerNoise.eval(i, j) * 2.0f;
+                if (y > 4.0f)
+                    y += layerNoise.eval(i, j) * 3.0f;
 
                 float maxY = Math.max(Math.max(Math.max(p0.y, p1.y), p2.y), p3.y);
                 float minY = Math.min(Math.min(Math.min(p0.y, p1.y), p2.y), p3.y);
 
-                Vector3f c = new Vector3f();
-                if (y > 20.0f) {
+                Vector3f c = new Vector3f(1.0f);
+                if (y > 22.0f) {
                     c.set(1.0f, 0.95f, 0.95f);
 
                     Vector3f p4 = new Vector3f(p0);
@@ -86,10 +106,10 @@ public class Terrain implements AutoCloseable {
                     Vector3f p6 = new Vector3f(p2);
                     Vector3f p7 = new Vector3f(p3);
 
-                    p0.y += 0.3f;
-                    p1.y += 0.3f;
-                    p2.y += 0.3f;
-                    p3.y += 0.3f;
+                    p0.y += 0.2f;
+                    p1.y += 0.2f;
+                    p2.y += 0.2f;
+                    p3.y += 0.2f;
 
                     vertices.add(p4);
                     vertices.add(new Vector3f(0.0f, 0.0f, 1.0f));
@@ -187,9 +207,9 @@ public class Terrain implements AutoCloseable {
                     vertices.add(new Vector3f(-1.0f, 0.0f, 0.0f));
                     vertices.add(c);
                 }
-                else if (y > 16.0f || maxY - minY > 1.7f)
+                else if (y > 18.0f || (maxY - minY > 1.0f && y > 12.0f))
                     c.set(0.4f, 0.35f, 0.35f);
-                else if (y > 5.0f) {
+                else if (y > 7.0f) {
                     c.set(0.4f, 0.6f, 0.1f);
 
                     Vector3f p4 = new Vector3f(p0);
@@ -197,10 +217,10 @@ public class Terrain implements AutoCloseable {
                     Vector3f p6 = new Vector3f(p2);
                     Vector3f p7 = new Vector3f(p3);
 
-                    p0.y += 0.3f;
-                    p1.y += 0.3f;
-                    p2.y += 0.3f;
-                    p3.y += 0.3f;
+                    p0.y += 0.2f;
+                    p1.y += 0.2f;
+                    p2.y += 0.2f;
+                    p3.y += 0.2f;
 
                     vertices.add(p4);
                     vertices.add(new Vector3f(0.0f, 0.0f, 1.0f));
@@ -298,7 +318,7 @@ public class Terrain implements AutoCloseable {
                     vertices.add(new Vector3f(-1.0f, 0.0f, 0.0f));
                     vertices.add(c);
                 }
-                else if (y != 3.0f)
+                else if (y != 4.0f)
                     c.set(0.76f, 0.7f, 0.5f);
                 else
                     c.set(0.0f, 0.4f, 1.0f);
@@ -365,18 +385,82 @@ public class Terrain implements AutoCloseable {
         glDeleteBuffers(vbo);
     }
 
-    private float sample(float x, float y) {
+    public float sample(float x, float y) {
         int x0 = Math.min(Math.max((int) Math.floor(x), 0), size - 1);
         int x1 = Math.min(Math.max((int) Math.ceil(x), 0), size - 1);
         int y0 = Math.min(Math.max((int) Math.floor(y), 0), size - 1);
         int y1 = Math.min(Math.max((int) Math.ceil(y), 0), size - 1);
 
-        float tx = Math.min(x - x0, 1.0f);
-        float ty = Math.min(x - x0, 1.0f);
+        float u = Math.min(x - x0, 1.0f);
+        float v = Math.min(x - x0, 1.0f);
 
-        float l = (1.0f - ty) * heightmap[x0][y0] + ty * heightmap[x0][y1];
-        float r = (1.0f - ty) * heightmap[x1][y0] + ty * heightmap[x1][y1];
-        return (1.0f - tx) * l + tx * r;
+        float l = (1.0f - v) * heightmap[x0][y0] + v * heightmap[x0][y1];
+        float r = (1.0f - v) * heightmap[x1][y0] + v * heightmap[x1][y1];
+        return (1.0f - u) * l + u * r;
+    }
+
+    public Vector2f gradient(float x, float y) {
+        int x0 = Math.min(Math.max((int) Math.floor(x), 0), size - 1);
+        int x1 = Math.min(Math.max((int) Math.ceil(x), 0), size - 1);
+        int y0 = Math.min(Math.max((int) Math.floor(y), 0), size - 1);
+        int y1 = Math.min(Math.max((int) Math.ceil(y), 0), size - 1);
+
+        float u = Math.min(x - x0, 1.0f);
+        float v = Math.min(x - x0, 1.0f);
+
+        Vector2f g = new Vector2f();
+        g.x = (heightmap[x1][y0] - heightmap[x0][y0]) * (1.0f - v) + (heightmap[x1][y1] - heightmap[x0][y1]) * v;
+        g.y = (heightmap[x0][y1] - heightmap[x0][y0]) * (1.0f - u) + (heightmap[x1][y1] - heightmap[x1][y0]) * u;
+        return g;
+    }
+
+    public void deposit(float x, float y, float amt) {
+        if (x < 0.0f || x >= size - 1 || y < 0.0f || y >= size - 1)
+            return;
+
+        float u = x - (int) x;
+        float v = y - (int) y;
+
+//        float leftWeight = 1.0f - u;
+//        float rightWeight = u;
+//
+//        float bottomWeight = 1.0f - v;
+//        float topWeight = v;
+
+        heightmap[(int) x][(int) y] += amt * (1.0f - u) * (1.0f - v);
+        heightmap[(int) x][(int) y + 1] += amt * (1.0f - u) * v;
+
+        heightmap[(int) x + 1][(int) y] += amt * u * (1.0f - v);
+        heightmap[(int) x + 1][(int) y + 1] += amt * u * v;
+    }
+
+    public void erode(float x, float y, float r, float amt) {
+        float normalizationFactor = 0.0f;
+
+        for (int i = (int) (x - r); i <= (int) (x + r); ++i) {
+            if (i < 0 || i >= size)
+                continue;
+
+            for (int j = (int) (y - r); j <= (int) (y + r); ++j) {
+                if (j < 0 || j >= size)
+                    continue;
+
+                normalizationFactor += Math.max(0.0f, r - Vector2f.distance(i, j, x, y));
+            }
+        }
+
+        for (int i = (int) (x - r); i <= (int) (x + r); ++i) {
+            if (i < 0 || i >= size)
+                continue;
+
+            for (int j = (int) (y - r); j <= (int) (y + r); ++j) {
+                if (j < 0 || j >= size)
+                    continue;
+
+                float w = Math.max(0.0f, r - Vector2f.distance(i, j, x, y));
+                heightmap[i][j] -= amt * w / normalizationFactor;
+            }
+        }
     }
 
     private float blurSample(float x, float y, float r) {
